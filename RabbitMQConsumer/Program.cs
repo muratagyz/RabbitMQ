8e68f6bd8b91b7aa776668ce0bf7d90772ae81d5
Murat AĞYÜZ <57285600+muratagyz@users.noreply.github.com>
2023-05-24 19:06:59 +0300

    Add project files.

---
+++ RabbitMQConsumer/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+#region Default : Round-Robin Dispatching
+//Connection
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://omqimsrq:4loIcS64HHTLB04sDnzMtji9C_5GYgIa@woodpecker.rmq.cloudamqp.com/omqimsrq");
+
+//Connection activation and channel opening
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//Queue creating
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+//Queue message reading
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: "example-queue", autoAck: false, consumer);
+consumer.Received += (sender, e) =>
+{
+    //Queue get messages
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+};
+
+Console.Read();
+#endregion
+
+#region Message Acknowledgement
+////Connection
+//ConnectionFactory factory = new();
+//factory.Uri = new("amqps://omqimsrq:4loIcS64HHTLB04sDnzMtji9C_5GYgIa@woodpecker.rmq.cloudamqp.com/omqimsrq");
+
+////Connection activation and channel opening
+//using IConnection connection = factory.CreateConnection();
+//using IModel channel = connection.CreateModel();
+
+////Queue creating
+//channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+////Queue message reading
+//EventingBasicConsumer consumer = new(channel);
+//channel.BasicConsume(queue: "example-queue", autoAck: false, consumer);
+//consumer.Received += (sender, e) =>
+//{
+//    //Queue get messages
+//    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+//    Task.Delay(3000);
+
+//    channel.BasicAck(e.DeliveryTag, multiple: false);
+//};
+
+//Console.Read();
+#endregion
---
+++ RabbitMQPublisher/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+//Connection
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://omqimsrq:4loIcS64HHTLB04sDnzMtji9C_5GYgIa@woodpecker.rmq.cloudamqp.com/omqimsrq");
+
+//Connection activation and channel opening
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//Queue creating
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+//Queue Message Send
+byte[] message = Encoding.UTF8.GetBytes("Hello");
+
+//Default Exchange Send
+channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message);
+
+Console.Read();
+
+#region Message Durability
+////Connection
+//ConnectionFactory factory = new();
+//factory.Uri = new("");
+
+////Connection activation and channel opening
+//using IConnection connection = factory.CreateConnection();
+//using IModel channel = connection.CreateModel();
+
+//IBasicProperties basicProperties = channel.CreateBasicProperties();
+//basicProperties.Persistent = true;
+////Queue creating
+//channel.QueueDeclare(queue: "example-queue", exclusive: false, durable: true);
+
+////Queue Message Send
+//byte[] message = Encoding.UTF8.GetBytes("Hello");
+
+////Default Exchange Send
+//channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message, basicProperties: basicProperties);
+
+//Console.Read();
+#endregion
